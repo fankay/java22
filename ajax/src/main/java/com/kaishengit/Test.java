5fffdef0ca354fa780573fda4253a069b441e647
fankay <fankai@kaishengit.com>
2016-12-08 16:57:26 +0800

    演示JSOUP

--- ajax/src/main/java/com/kaishengit/Test.java
+++ ajax/src/main/java/com/kaishengit/Test.java
@@ -1,136 +1,63 @@
 package com.kaishengit;
 
-import com.google.gson.Gson;
-import com.kaishengit.entity.User;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Test {
 
     public static void main(String[] args) throws IOException {
 
-        User user = new User(1,"李四","北京");
-        User user1 = new User(12,"王明明","上海");
-        User user2 = new User(23,"赵丽丽","郑州");
+       for (int j = 1;j < 3;j++) {
+           Document document = Jsoup
+                   .connect("http://www.topit.me/?p="+j)
+                   .cookie("is_click","1")
+                   .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36")
+                   .get();
 
-        List<User> userList = Arrays.asList(user,user1,user2);
+           Elements  elements = document.select("#content .catalog .e > a");
 
-        Gson gson = new Gson();
-        String json = gson.toJson(userList);
+           for(int i = 0;i < elements.size();i++) {
+               Element element = elements.get(i);
+               String href = element.attr("href");
 
-        System.out.println(json);
+               System.out.println(href);
 
+               Document bigDocument = Jsoup.connect(href)
+                       .cookie("is_click","1")
+                       .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36")
+                       .get();
+               Element img = bigDocument.select("#content #item-tip img").first();
+               String imgSrc = img.attr("src");
 
-        /*List<String> names = Arrays.asList("jack","rose","alex");
-        String json = new Gson().toJson(names);
+               CloseableHttpClient httpClient = HttpClients.createDefault();
+               HttpGet httpGet = new HttpGet(imgSrc);
+               HttpResponse response = httpClient.execute(httpGet);
+               if(response.getStatusLine().getStatusCode() == 200) {
+                   InputStream inputStream = response.getEntity().getContent();
+                   String imageName = imgSrc.substring(imgSrc.lastIndexOf("/"));
+                   FileOutputStream outputStream = new FileOutputStream("D:/topit/"+imageName);
+                   IOUtils.copy(inputStream,outputStream);
+                   outputStream.flush();
+                   outputStream.close();
+                   inputStream.close();
+               } else {
+                   System.out.println("ERROR:" + response.getStatusLine().getStatusCode());
+               }
 
-        System.out.println(json);*/
-
-
-        /*User user = new User(1,"李四","北京");
-
-        String json = new Gson().toJson(user);
-        System.out.println(json);*/
-
-
-
-
-       /* CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://localhost:8080/save");
-
-        List<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("userName","jack"));
-        params.add(new BasicNameValuePair("address","usa"));
-
-        httpPost.setEntity(new UrlEncodedFormEntity(params));
-
-        for(int i = 0;i < 5;i++) {
-            httpClient.execute(httpPost);
-        }*/
-
-
-
-
-
-       /* CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("http://ww1.sinaimg.cn/mw690/824de770jw1epwcnivby6j20go0p00x4.jpg");
-        HttpResponse response = httpClient.execute(httpGet);
-
-        if(response.getStatusLine().getStatusCode() == 200) {
-            InputStream inputStream = response.getEntity().getContent();
-            OutputStream outputStream = new FileOutputStream("D:/x.jpg");
-
-            IOUtils.copy(inputStream,outputStream);
-
-            outputStream.flush();
-            outputStream.close();
-            inputStream.close();
-
-        }
-
-        httpClient.close();*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       /* //创建了一个可以发出请求的客户端
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        //创建一个get请求方式
-        HttpGet httpGet = new HttpGet("http://www.kaishengit.com");
-        //发出请求并接受服务端的响应
-        HttpResponse response = httpClient.execute(httpGet);
-
-        //获取HTTP响应的状态码
-        int statusCode = response.getStatusLine().getStatusCode();
-
-        if(statusCode == 200) {
-            //获取响应输入流
-            InputStream inputStream = response.getEntity().getContent();
-
-            String result = IOUtils.toString(inputStream,"UTF-8");
-
-            inputStream.close();
-
-            System.out.println(result);
-
-
-
-        } else {
-            System.out.println("服务器异常:" + statusCode);
-        }
-
-        httpClient.close();*/
-
+           }
+       }
     }
 }
 
