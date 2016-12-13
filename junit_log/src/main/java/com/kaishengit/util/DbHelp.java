5abe3aab937027d72abde140283869b003e71728
fankay <fankai@kaishengit.com>
2016-12-13 10:05:05 +0800

    演示log4j更多的配置以及slf4j和Logback的使用

--- junit_log/src/main/java/com/kaishengit/util/DbHelp.java
+++ junit_log/src/main/java/com/kaishengit/util/DbHelp.java
@@ -7,25 +7,25 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 
 import com.kaishengit.exception.DataAccessException;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DbHelp {
 
-    private static Logger logger = Logger.getLogger(DbHelp.class);
+    private static Logger logger = LoggerFactory.getLogger(DbHelp.class);
 
     private static Connection getConnection() {
         return ConnectionManager.getConnection();
     }
 
     public static void update(String sql,Object... params) throws DataAccessException {
-
         try {
             QueryRunner queryRunner = new QueryRunner(ConnectionManager.getDataSource());
             queryRunner.update(sql, params);
 
-            logger.debug("SQL: " + sql);
+            logger.debug("SQL: {}",sql);
         } catch (SQLException ex) {
-            logger.error("执行"+ sql + "异常");
+            logger.error("执行{}异常",sql);
             throw new DataAccessException("执行"+ sql + "异常",ex);
         }
     }
@@ -36,10 +36,10 @@ public class DbHelp {
         try {
             T t = queryRunner.query(sql,handler,params);
 
-            logger.debug("SQL: " + sql);
+            logger.debug("SQL: {}");
             return t;
         } catch (SQLException e) {
-            logger.error("执行"+ sql + "异常");
+            logger.error("执行{}异常",sql);
             throw new DataAccessException("执行"+ sql + "异常",e);
         }
     }
--- junit_log/src/test/java/com/kaishengit/dao/Log4jTestCase.java
+++ junit_log/src/test/java/com/kaishengit/dao/Log4jTestCase.java
@@ -1,20 +1,20 @@
 package com.kaishengit.dao;
 
-import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Log4jTestCase {
 
     @Test
     public void testLog() {
-        Logger logger = Logger.getLogger(Log4jTestCase.class);
+        Logger logger = LoggerFactory.getLogger(Log4jTestCase.class);
 
-        logger.trace("trace message");
+        logger.trace("{}-{} trace message","tom","hello");
         logger.debug("debug message");
         logger.info("info message");
         logger.warn("warn message");
         logger.error("error message");
-        logger.fatal("fatal message");
 
     }
 }
