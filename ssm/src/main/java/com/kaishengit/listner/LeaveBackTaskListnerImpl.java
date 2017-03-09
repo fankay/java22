70f28c1572d20e7a3603b9906aa8e0eab21e96b0
jimi1989 <jinjianghao@kaishengit.com>
2017-03-09 10:37:39 +0800

    配置actitivi开发环境

---
+++ ssm/src/main/java/com/kaishengit/listner/LeaveBackTaskListnerImpl.java
@@ -0,0 +1,14 @@
+package com.kaishengit.listner;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+/**
+ * Created by jimi_jin on 2017-3-9.
+ */
+public class LeaveBackTaskListnerImpl implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+
+    }
+}
